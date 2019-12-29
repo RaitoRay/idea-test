a2bf9a829c332fb63b8a6385a85ae05af3fc5abd
guolingjiang <guolingjiang@qq.com>
2019-12-29 15:34:58 +0800

    xiugai

--- demo/src/com/ray/Main.java
+++ demo/src/com/ray/Main.java
@@ -8,5 +8,9 @@ public class Main {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+        test();
+    }
+    public static void test(){
+        System.out.println("test");
     }
 }
