4bde7ab993ca0eced8e80dfda47486226578db30
guolingjiang <guolingjiang@qq.com>
2019-12-29 15:26:03 +0800

    添加git，demo

---
+++ demo/src/com/ray/Main.java
@@ -0,0 +1,12 @@
+package com.ray;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
